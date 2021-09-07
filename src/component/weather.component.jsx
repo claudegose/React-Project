4047401e77c9414b9f685363ad024b6f5e46a647
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-07 11:16:14 +0200

    Min/Max Temperature Component Created

--- src/component/weather.component.jsx
+++ src/component/weather.component.jsx
@@ -7,12 +7,28 @@ const Weather = () => {
         <div className="cards">
             <h1>London</h1>
             <h5 className="py-4">
-                <i className="wi wi-day-sunny"></i>
+                <i className="wi wi-day-sunny display-1"></i>
 
             </h5>
+            <h1 className="oy-2">25&deg;</h1>
         </div>
         </div>
     );
 };
+
+function minmaxTemp (min, max){
+    return (
+        <h3>
+            <span className="px-4">{min}&deg;</span>
+            <span className="px-4">{max}&deg;</span>
+
+        </h3>
+    )
+
+}
+
+
+
+
 
 export default Weather;
