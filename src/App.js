d64c7abe47825d2042366af0952496be62fad3cb
claudegose <84382636+claudegose@users.noreply.github.com>
2021-09-07 12:00:34 +0200

    SetStates for Current Temp, Location, Forcast

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ const API_key = "3a6bc0c0cb362d0812c688e02eb124f8"
 class App extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            city : undefined,
+            country: undefined
+        };
         this.getWeather();
     }
 
@@ -20,15 +23,22 @@ const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=L
 
 const response = await api_call.json();
 
-console.log(response)
+console.log(response);
 
-}
+this.setState({
+    city: response.name,
+    country: response.sys.country
+
+
+})
+
+};
 
 
     render() {
         return(
             <div className="App">
-                <Weather />
+                <Weather city={this.state.city} country={this.state.country}/>
 
             </div>
         )
